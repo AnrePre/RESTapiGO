a092f652a124366a582fb010d0e67779b5770a16
Anre Pretorius <170444398+AnrePre@users.noreply.github.com>
2025-07-12 14:38:32 +0200

    created client for testing

---
+++ client.go
@@ -0,0 +1,30 @@
+package restapi
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func client() {
+	//create a new http client
+	client := &http.Client{}
+
+	//resp, err := client.Get("https://jsonplaceholder.typicode.com/posts/1")
+	resp, err := client.Get("https://swapi.dev/api/people/1")
+	if err != nil {
+		fmt.Println("Error making GET request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	//Read and print the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+
+	fmt.Println(string(body))
+	fmt.Println(body)
+}
